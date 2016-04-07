f234a4b580f2ae57e16529012779cfc63b26e61c
mkreiser <mkreise2@illinois.edu>
2016-04-06 19:23:18 -0500

    Publish front end

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cs410VirtualLabApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cs410VirtualLabApp
+ */
+angular.module('cs410VirtualLabApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
