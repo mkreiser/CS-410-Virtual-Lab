b53b14b47c309c7dc5e6ae19ba2f5424995adf49
mkreiser <mkreise2@illinois.edu>
2016-05-08 14:35:52 -0500

    Add query runner

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -4,6 +4,7 @@ angular.module('cs410VirtualLabApp')
 
 .constant('RANKER_INFO', {
 	'Okapi BM25': {
+		requestName: 'bm25',
 		parameters: [{
 			name: 'k1',
 			default: 1.2
@@ -16,24 +17,28 @@ angular.module('cs410VirtualLabApp')
 		}]
 	},
 	'Pivoted Length Normalization': {
+		requestName: 'pivoted-length',
 		parameters: [{
 			name: 's',
 			default: 0.2
 		}]
 	},
 	'Absolute Discount Smoothing': {
+		requestName: 'absolute-discount',
 		parameters: [{
 			name: 'delta',
 			default: 0.7
 		}]
 	},
 	'Jelinek-Mercer Smoothing': {
+		requestName: 'jelinek-mercer',
 		parameters: [{
 			name: 'lambda',
 			default: 0.7
 		}]
 	},
 	'Dirichlet-Prior Smoothing': {
+		requestName: 'dirichlet-prior',
 		parameters: [{
 			name: 'mu',
 			default: 2000.0
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,32 @@
 
 angular.module('cs410VirtualLabApp')
 
-.controller('mainCtrl', function($scope, RANKER_INFO) {
+.controller('mainCtrl', function($http, $scope, RANKER_INFO) {
 
 	$scope.$watch('ranker', function(newRanker) {
 		$scope.rankerInfo = RANKER_INFO[newRanker];
 		$scope.parameters = {};
 	});
+
+	$scope.sendQueryRunner = function() {
+		if ($scope.query === '' || $scope.query === undefined || $scope.ranker === undefined) {
+			return;
+		}
+
+		var requestObj = {
+			query: $scope.query,
+			ranker: $scope.rankerInfo.requestName
+		};
+
+		angular.forEach($scope.rankerInfo.parameters, function(p) {
+			requestObj[p.name] = $scope.parameters[p.name];
+		});
+
+		console.log(requestObj);
+
+		$http.post('http://localhost:9001/', requestObj)
+			.then(function(response) {
+				console.log(response);
+			});
+	};
 });
